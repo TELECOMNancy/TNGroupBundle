20109a4a7b49ba8ea5bfcf4bb13cf241360674cb
Thibaut <thibaut.smith@telecomnancy.net>
2014-02-01 15:00:01 +0100

    Fix UserFactory and Listener.

--- DependencyInjection/Security/Factory/TNUserFactory.php
+++ DependencyInjection/Security/Factory/TNUserFactory.php
@@ -1,39 +1,73 @@
 <?php
 namespace Videl\TNGroupBundle\DependencyInjection\Security\Factory;
-
+ 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\DefinitionDecorator;
 use Symfony\Component\Config\Definition\Builder\NodeDefinition;
-use Symfony\Bundle\SecurityBundle\DependencyInjection\Security\Factory\SecurityFactoryInterface;
-
-class TNUserFactory implements SecurityFactoryInterface
+use Symfony\Bundle\SecurityBundle\DependencyInjection\Security\Factory\AbstractFactory;
+ 
+class TNUserFactory extends AbstractFactory
 {
-    public function create(ContainerBuilder $container, $id, $config, $userProvider, $defaultEntryPoint)
-    {
-        $providerId = 'security.authentication.provider.tnuser.'.$id;
-        $container
-            ->setDefinition($providerId, new DefinitionDecorator('tnuser.security.authentication.provider'))
-            ->replaceArgument(0, new Reference($userProvider))
-        ;
+ 
+/**
+* {@inheritdoc}
+*/
+protected function createAuthProvider(ContainerBuilder $container, $id, $config, $userProviderId)
+{
+$provider = 'security.authentication.provider.tnuser.'.$id;
+$container
+->setDefinition($provider, new DefinitionDecorator('security.authentication.provider.tnuser'))
+->replaceArgument(0, new Reference($userProviderId))
+;
+ 
+return $provider;
+}
+ 
+ 
+/**
+* {@inheritdoc}
+*/
+protected function createListener($container, $id, $config, $userProvider)
+{
+$listenerId = $this->getListenerId();
+$listener = new DefinitionDecorator($listenerId);
+$listener->replaceArgument(4, $id);
+$listener->replaceArgument(5, new Reference($this->createAuthenticationSuccessHandler($container, $id, $config)));
+$listener->replaceArgument(6, new Reference($this->createAuthenticationFailureHandler($container, $id, $config)));
+$listener->replaceArgument(7, array_intersect_key($config, $this->options));
+ 
+$listenerId .= '.'.$id;
+$container->setDefinition($listenerId, $listener);
+ 
+return $listenerId;
+}
+ 
+ 
+/**
+* {@inheritdoc}
+*/
+protected function getListenerId()
+{
+return 'security.authentication.listener.tnuser';
+}
+ 
+ 
+/**
+* {@inheritdoc}
+*/
+protected function isRememberMeAware($config)
+{
+return false;
+}
+public function getPosition()
+{
+return 'form';
+}
+public function getKey()
+{
+return 'tnuser';
+}
 
-        $listenerId = 'security.authentication.listener.tnuser.'.$id;
-        $listener = $container->setDefinition($listenerId, new DefinitionDecorator('tnuser.security.authentication.listener'));
 
-        return array($providerId, $listenerId, $defaultEntryPoint);
-    }
-
-    public function getPosition()
-    {
-        return 'pre_auth';
-    }
-
-    public function getKey()
-    {
-        return 'tnuser';
-    }
-
-    public function addConfiguration(NodeDefinition $node)
-    {
-    }
 }
--- Security/Firewall/TNUserListener.php
+++ Security/Firewall/TNUserListener.php
@@ -4,68 +4,65 @@ namespace Videl\TNGroupBundle\Security\Firewall;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\Security\Http\Firewall\ListenerInterface;
+use Symfony\Component\Security\Http\Firewall\AbstractAuthenticationListener;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
 use Videl\TNGroupBundle\Security\Authentication\Token\TNUserToken;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bridge\Monolog\Logger;
 
-class TNUserListener implements ListenerInterface
+class TNUserListener extends AbstractAuthenticationListener
 {
-    protected $securityContext;
-    protected $authenticationManager;
     protected $logger;
 
-    public function __construct(SecurityContextInterface $securityContext, AuthenticationManagerInterface $authenticationManager, Logger $logger)
-    {
-        $this->securityContext = $securityContext;
-        $this->authenticationManager = $authenticationManager;
-        $this->logger = $logger;
-    }
+    // public function __construct(
+    //     SecurityContextInterface $securityContext,
+    //     AuthenticationManagerInterface $authenticationManager,
+    //     SessionAuthenticationStrategyInterface $sessionStrategy,
+    //     HttpUtils $httpUtils,
+    //     $providerKey,
+    //     AuthenticationSuccessHandlerInterface $successHandler,
+    //     AuthenticationFailureHandlerInterface $failureHandler,
+    //     array $options = array(),
+    //     LoggerInterface $logger = null,
+    //     EventDispatcherInterface $dispatcher = null
+    // ) {
+    //     parent::__construct(
+    //         $securityContext,
+    //         $authenticationManager,
+    //         $sessionStrategy,
+    //         $httpUtils,
+    //         $providerKey,
+    //         $successHandler,
+    //         $failureHandler,
+    //         array_merge(
+    //             array(
+    //                 'username_parameter' => '_username',
+    //                 'password_parameter' => '_password',
+    //                 'csrf_parameter'     => '_csrf_token',
+    //                 'intention'          => 'authenticate',
+    //                 'post_only'          => true,
+    //             ),
+    //             $options
+    //         ),
+    //         $logger,
+    //         $dispatcher
+    //     );
 
-    public function handle(GetResponseEvent $event)
-    {
-        $request = $event->getRequest();
+    // }
 
-        // $wsseRegex = '/UsernameToken Username="([^"]+)", PasswordDigest="([^"]+)", Nonce="([^"]+)", Created="([^"]+)"/';
-        // if (!$request->headers->has('x-wsse') || 1 !== preg_match($wsseRegex, $request->headers->get('x-wsse'), $matches)) {
-        //     return;
-        // }
-        // 
-        $this->logger->info("Méthode : " . $request->getMethod());
+    public function attemptAuthentication(Request $request)
+    {
+        $this->logger->info("Abstract class : " . $request->getMethod());
 
         $token = new TNUserToken();
-        $token->setUser("tn.net");
 
-        $token->email = $request->get("_username");
-
-        try {
-            $authToken = $this->authenticationManager->authenticate($token);
-            $this->securityContext->setToken($authToken);
-
-            return;
-        } catch (AuthenticationException $failed) {
-            // ... you might log something here
-            $this->logger->info("ERREUR: L'auth a echoué!");
-
-            // To deny the authentication clear the token. This will redirect to the login page.
-            // Make sure to only clear your token, not those of other authentication listeners.
-            $token = $this->securityContext->getToken();
-            if ($token instanceof TNUserToken /*&& $this->providerKey === $token->getProviderKey()*/) {
-                $this->securityContext->setToken(null);
-            }
+        if($request->getMethod() != "POST")
             return;
 
-            // Deny authentication with a '403 Forbidden' HTTP response
-            $response = new Response();
-            $response->setStatusCode(Response::HTTP_FORBIDDEN);
-            $event->setResponse($response);
+        $token->setUser("tnuser");
 
-        }
-
-        // By default deny authorization
-        $response = new Response();
-        $response->setStatusCode(Response::HTTP_FORBIDDEN);
-        $event->setResponse($response);
+        $token->email = $request->get("_username") . '...';
     }
 }
